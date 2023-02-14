12138b344766673f1a3d722202e3e3da59eb23d9
Ivan Krizsan <ivan@ivankrizsan.se>
2023-02-14 21:49:40 +0100

    Deleted old warehouse module code

--- src/main/java/se/ivankrizsan/monolithmicroservices/modules/warehouse/exceptions/WarehouseException.java
+++
@@ -1,9 +0,0 @@
-package se.ivankrizsan.monolithmicroservices.modules.warehouse.exceptions;
-
-/**
- * Base class for exceptions related to the {@code WarehouseService}.
- *
- * @author Ivan Krizsan
- */
-public class WarehouseException extends RuntimeException {
-}
